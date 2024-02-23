d3231222bb0488172c0150c4323f3895b0c7fd13
Razibul Islam <dev.ronib@gmail.com>
2024-02-23 11:38:01 +0600

    Code optimized

--- public/cn/colorPalette/app.js
+++ public/cn/colorPalette/app.js
@@ -1,21 +1,20 @@
-const container = document.querySelector(".container");
+const container = document.getElementById("container");
 const refreshBtn = document.querySelector(".refresh-btn");
 
-const maxPaletteBoxes = 32;
+const maxPaletteBoxes = 48;
 
 const generatePalette = () => {
-  container.innerHTML = ""; // clearing the container
+  container.innerHTML = "";
   for (let i = 0; i < maxPaletteBoxes; i++) {
-    // generating a random hex color code
     let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
     randomHex = `#${randomHex.padStart(6, "0")}`;
 
-    // creating a new 'li' element and inserting it to the container
-    const color = document.createElement("li");
-    color.classList.add("color");
-    color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
-                           <span class="hex-value">${randomHex}</span>`;
-    // adding click event to current li element to copy the color
+    const color = document.createElement("div");
+
+    color.classList.add("min-w-48", "w-auto");
+
+    color.innerHTML = `<div class='m-2 p-2 sm:p-1 text-center bg-white dark:bg-slate-700 hover:brightness-125 rounded-md overflow-hidden cursor-pointer'><div class="w-full h-44 sm:h-48 border-2 border-slate-400 dark:border-slate-600 rounded-md" style="background: ${randomHex}"></div>
+    <span class="hex-value block mt-3 mb-2 text-lg md:font-medium uppercase select-none">${randomHex}</span></div>`;
     color.addEventListener("click", () => copyColor(color, randomHex));
     container.appendChild(color);
   }
@@ -24,15 +23,13 @@ generatePalette();
 
 const copyColor = (elem, hexVal) => {
   const colorElement = elem.querySelector(".hex-value");
-  // Copying the hex value, updating the text to copied,
-  // and changing text back to original hex value after 1 second
   navigator.clipboard
     .writeText(hexVal)
     .then(() => {
       colorElement.innerText = "Copied";
       setTimeout(() => (colorElement.innerText = hexVal), 1000);
     })
-    .catch(() => alert("Failed to copy the color code!")); // showing alert if color can't be copied
+    .catch(() => alert("Failed to copy the color code!"));
 };
 
 refreshBtn.addEventListener("click", generatePalette);
