2dae057f02b63bf84ceb0ce327a3f5a4dc70a0fc
Razibul Islam <dev.ronib@gmail.com>
2024-03-15 03:04:03 +0600

    SSL Checker Updated

--- public/lib/sslChecker/app.js
+++ public/lib/sslChecker/app.js
@@ -1,59 +1,51 @@
+const fetchData = async (domain) => {
+  try {
+    const res = await fetch(`https://sslcheckerapi.vercel.app/${domain}`); // my own api
+    const data = await res.json();
+    console.log(data);
+    displaySSLInformation(data);
+  } catch (error) {
+    result.innerHTML = `<div class="text-red-800">${error.message}</div>`;
+  }
+};
+
 document.getElementById("checkBtn").addEventListener("click", function () {
-  const domain = document.getElementById("domain").value.trim();
+  const domain = document.getElementById("domain").value;
   if (domain === "") {
     Swal.fire({
       icon: "error",
       title: "Oops...",
-      text: "Enter a valid domain!",
+      text: "Domain is Empty!",
     });
     return;
   }
 
-  // Clear previous results
   clearResult();
 
-  const apiUrl = `https://api.ssllabs.com/api/v3/analyze?host=${domain}`;
-
-  fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("ERROR: Failed to fetch SSL information.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      displaySSLInformation(data);
-    })
-    .catch((error) => {
-      console.error(error);
-      displayErrorMessage("ERROR: Failed to fetch SSL information.");
-    });
+  fetchData(domain);
 });
 
+const result = document.getElementById("result");
+
 function displaySSLInformation(data) {
-  const result = document.getElementById("result");
-  const grade = data.endpoints[0].grade;
-
-  result.innerHTML = `<div class="bg-green-100 p-4 rounded-md">
-<p class="text-lg font-bold">SSL Information for ${data.host}:</p>
-<p class="mb-2"><strong>Grade:</strong> ${grade}</p>
-<p class="mb-2"><strong>Details:</strong> ${data.endpoints[0].details}</p>
-<p class="mb-2"><strong>Protocol:</strong> ${data.endpoints[0].details.protocols[0]}</p>
-<p class="mb-2"><strong>Cipher Suite:</strong> ${data.endpoints[0].details.suites[0].name}</p>
-</div>`;
-}
-
-function displayErrorMessage(message) {
-  const result = document.getElementById("result");
-  result.innerHTML = `<p class="text-red-500">${message}</p>`;
+  result.innerHTML = `
+  <div>
+    <div class="dark:bg-slate-700 text-black dark:text-white text-center text-2xl font-bold py-2 px-4 md:col-span-2  border rounded-t-md">SSL Information</div>
+    <ul class="*:border *:border-t-0 *:p-2">
+      <li><strong>Domain Name:</strong> ${data.domain}</li>
+      <li><strong>SSL Provider:</strong> ${data.provider}</li>
+      <li><strong>Issued:</strong> ${data.issued}</li>
+      <li><strong>Expires:</strong> ${data.expires}</li>
+      <li><strong>Expires in:</strong> ${data.daysLeft} Days</li>
+    </ul>
+  </div>`;
 }
 
 function clearResult() {
-  const result = document.getElementById("result");
   result.innerHTML = "";
 }
 
 function reset() {
   document.getElementById("domain").value = "";
-  document.getElementById("result").innerHTML = "";
+  clearResult();
 }
